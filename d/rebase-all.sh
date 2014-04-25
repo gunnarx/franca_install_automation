d925360d33b7b91b4047a108cda6824ac19de601
Gandaman <github.10.gand@dfgh.net>
2014-04-25 13:26:51 +0200

    Merge upstream after rebase to avoid push conflict

--- d/rebase-all.sh
+++ d/rebase-all.sh
@@ -9,6 +9,7 @@ for br in $(git branch --list | grep -v master | sed 's/\*//' ) ; do
    [ -n "$br" ] && {
       git checkout $br || die
       git rebase master || die
+      git pull origin || die
    }
 done
 
